829f647455d14bddbcc95e950fe886c1133f6167
Dima1234566 <fe9eee@i.ua>
2018-11-22 10:57:25 +0200

    Update MainActivity.java

--- lab5/app/src/main/java/e/dimas/lab5/MainActivity.java
+++ lab5/app/src/main/java/e/dimas/lab5/MainActivity.java
@@ -16,7 +16,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
     public class MainActivity extends AppCompatActivity {
-        ListView listView;
+        privat ListView listView;
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
