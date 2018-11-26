69801f69501e1b52ddbeb8c8cf350ea99ed7f84a
Dima1234566 <fe9eee@i.ua>
2018-11-26 11:56:45 +0200

    lab6
    lab6

---
+++ lab6/app/src/main/java/e/dimas/lab5/Photo.java
@@ -0,0 +1,15 @@
+package e.dimas.lab5;
+
+
+public class Photo {
+
+    private final String name;
+
+    public Photo(String name) {
+        this.name =  name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
